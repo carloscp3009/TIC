237fce4ec58bcab5fd98ffb77b24b0e99940b2b0
Carlos <carloscp3009@hotmail.com>
2019-11-19 10:50:02 -0500

    SR fisrt version

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,4 +10,33 @@ router.get('/selection', (req,res)=> {
 router.get('/plastic', (req,res)=> {
   res.render('plastic')
 });
+router.get('/paper', (req,res)=> {
+  res.render('paper')
+});
+router.get('/glass', (req,res)=> {
+  res.render('glass')
+});
+router.get('/organic', (req,res)=> {
+  res.render('organic')
+});
+router.get('/thanks/:img', (req,res)=> {
+  let img = req.params.img;
+  res.render('thanks',{img})
+});
+router.get('/congrats', (req,res)=> {
+  res.render('congrats')
+});
+router.get('/signin', (req,res)=> {
+  res.render('signin')
+});
+router.get('/signup', (req,res)=> {
+  res.render('signup')
+});
+router.post('/signin', (req,res)=> {
+  res.render('selection')
+});
+router.post('/signup', (req,res)=> {
+  res.render('selection')
+});
+
 module.exports = router;
